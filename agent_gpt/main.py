85892862ed10fceb4e94b6b75e3732c29a8b397b
Ryan Elton <elston.ryan@gmail.com>
2024-03-26 07:27:51 -0700

    Add setup to init

--- agent_gpt/main.py
+++ agent_gpt/main.py
@@ -6,21 +6,6 @@ import click
 open_ai_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=open_ai_key)
 
-# Initialize and setup
-AGENTS_FOLDER = os.path.expanduser("~/.gpt/agents")
-DEFAULT_AGENT = f"{AGENTS_FOLDER}/default.yaml"
-
-if not os.path.exists(AGENTS_FOLDER):
-    os.makedirs(AGENTS_FOLDER)
-    print(f"Created {AGENTS_FOLDER} folder.")
-
-if not os.path.exists(DEFAULT_AGENT):
-    with open(os.path.expanduser( DEFAULT_AGENT), "w") as file:
-        file.write("""system: >
-    You are a helpful LLM agent that always returns your response in Markdown format.""")
-    print(f"Created {DEFAULT_AGENT} file.")
-
-
 # Loading Agents in AGENTS_FOLDER
 agents = {}
 for filename in os.listdir(os.path.expanduser("~/.gpt/agents")):
