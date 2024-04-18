a30b90548cc3fed22fe257689efd094dd26b3abc
Ryan Elton <elston.ryan@gmail.com>
2024-04-17 23:25:54 -0600

    Clean up and add test for writter module

--- mark/writer.py
+++ mark/writer.py
@@ -1,14 +1,18 @@
 from mark import llm
+from IPython import embed
 
-def write_response(file_name, message, agent='default', model=llm.MODEL):
-    """
-    Append the GPT response to the given file.
-    """
-    with open(file_name, "a") as file:
-        content = f"""
+RESPONSE_TEMPLATE = """
 **GPT Response (model: {model}, agent: {agent})**
 {message}
 
 **User Response**
 """
+
+def write_response(file_name, message, agent='default', model=llm.MODEL):
+    """
+    Append the GPT response to the given file.
+    """
+    content = RESPONSE_TEMPLATE.format(model=model, agent=agent, message=message)
+    with open(file_name, "a") as file:
+        # embed()
         file.write(content)
