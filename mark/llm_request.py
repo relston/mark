a8e1ff5f3b845add1d68d7ddaf5430591ce1730b
Ryan Elton <elston.ryan@gmail.com>
2024-04-27 12:44:32 -0600

    Add basic error handling for missing local images

--- mark/llm_request.py
+++ mark/llm_request.py
@@ -32,13 +32,16 @@ class Image:
         if self._path.startswith("http"):
             return self._path 
         else:
-            base64_image = self._base64_encode()
-            return f"data:image/jpeg;base64,{base64_image}"
+            try:
+                base64_image = self._base64_encode()
+                return f"data:image/jpeg;base64,{base64_image}"
+            except FileNotFoundError as e:
+                print(f"Sorry, {e.filename} does not exist.")
 
     def _base64_encode(self):
         with open(self._path, "rb") as image_file:
             encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
-        return encoded_image
+            return encoded_image
 
 def parse_markdown_content(markdown_content):
     html_content = markdown(markdown_content)
